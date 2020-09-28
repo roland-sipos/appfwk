bb3cd750150a7f9a2515d2a39bdb9fbe8dcb178e
Roland Sipos <roland.sipos@cern.ch>
2020-09-28 10:31:18 +0200

    For now, dependencies are setup via variables

---
+++ setup-extra-deps.sh
@@ -0,0 +1,6 @@
+# This versions needs the following variables exported
+
+export PRODUCTS=/cvmfs/dune.opensciencegrid.org/dunedaq/DUNE/products_dev:$PRODUCTS
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/<ctrlnode-lib-dir>
+export CCM_INC_DIR=/<ctrlnode-include-dir>
+export PISTACHE_INC_DIR=/<pistache-include-dir>
